66300857c87b4bc1104b9d0c9f0660c5ca557826
Pandu Rendradjaja <218643070+pandu-apsis@users.noreply.github.com>
2025-12-01 23:30:37 -0800

    ++

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { ColorPicker } from './ColorPicker'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   function setColor(index: number, color: string) {
     document.querySelectorAll('svg path')[index].setAttribute('fill', color)
     document.querySelectorAll('svg path')[index].setAttribute('stroke', color)
